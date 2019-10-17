97a59fadb096f93a0a4a565f1a9fc092f992776a
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-10-18 00:10:29 +0300

    Fix

--- source/search.js
+++ source/search.js
@@ -1,58 +1,60 @@
 "use strict";
 window.substringSearch = function (options) {
 
-  options = options || {
-
-  };
+  options = options || {}
+  
+  options.weight = options.weight || 100;
+  options.multiply = options.multiply || true;
+  options.similar = options.similar || [];
 
   let _this = this,
-      results = [],
-      proto ={
-        weight: 0,
-        search: '',
-        res: '',
-        in: -1,
-        out: -1,
-        length: 0
-      };
+    results = [],
+    proto = {
+      weight: 0,
+      search: '',
+      res: '',
+      in: -1,
+      out: -1,
+      length: 0
+    };
 
 
   this.inString = function (substr, str) {
-    
+
     substr = '' + substr;
-    
+
     let wordsSearch = (substr.length) ? substr.trim().split(' ') : [''],
-        words = (str.length) ? str.trim().split(' ') : [''],
-        _res = {str: str},
-        result = [];
+      words = (str.length) ? str.trim().split(' ') : [''],
+      _res = {
+        str: str
+      },
+      result = [];
 
     if (substr.length) {
 
-        if (str.indexOf(' ') + 1){
+      if (substr.indexOf(' ') + 1) {
 
-          let fullMatch = search(substr, _res);
+        let fullMatch = search(substr, _res);
 
-          if (fullMatch.weight) fullMatch.weight = fullMatch.weight * words.length;
+        if (fullMatch.weight) fullMatch.weight = fullMatch.weight * words.length;
 
-          result = fullMatch;
+        result = fullMatch;
 
-        }
-      
-      
+      }
 
-      for ( var i = wordsSearch.length; i--; ) {
+      for (var i = wordsSearch.length; i--;) {
 
-        result = result.concat( search( wordsSearch[i], _res ) );
+        result = result.concat(search(wordsSearch[i], _res));
 
       }
 
       result = result.sort(compare);
 
     } else {
-      
+
       let tempItem = copyObject(proto);
-          tempItem.search = substr;
-          tempItem.res = _res;
+      tempItem.search = substr;
+      tempItem.res = _res;
 
       result.push(tempItem);
 
@@ -62,62 +64,67 @@ window.substringSearch = function (options) {
 
   }
 
-  this.inObject = function (substr, data, getKey) {
-    
+  this.inArray = function (substr, arr, getKey) {
+
     substr = '' + substr;
-    
+
     let result = [];
-    
-    for ( var i = data.length; i--; ){
-      
-      let str = getKey(data[i]),
-          _res = {obj: data[i], str: str},
-          _result = _this.inString(substr, str);
-      
-        _result.res = _res;
-      
-      result = result.concat(_result);
-          
+
+    for (var i = arr.length; i--;) {
+
+      let str = getKey(arr[i]),
+        _res = {
+          obj: arr[i],
+          str: str,
+          index: i
+        },
+        _result = _this.inString(substr, str);
+
+      for (var r = _result.length; r--;) {
+        
+        if (_result[r]) _result[r].res = _res;
+
+      }
+
+      if (_result.length) result = result.concat(_result);
+
     }
-    
+
     result = result.sort(compare);
-    
+
     return result;
 
   }
 
-
   function search(substr, _res) {
 
     let search = substr.toLowerCase().trim(),
-        str = _res.str,
-        normalStr = str.toLowerCase(),
-        pos = normalStr.indexOf(search),
-        length = substr.length * 10,
-        result = [];
+      str = _res.str,
+      normalStr = str.toLowerCase(),
+      pos = normalStr.indexOf(search),
+      length = substr.length * (options.weight / 10),
+      result = [];
 
     while (true) {
 
       let tempItem = copyObject(proto);
-          tempItem.search = substr;
-          tempItem.res = _res;
-      
+      tempItem.search = substr;
+      tempItem.res = _res;
+
       let foundPos = normalStr.indexOf(search, pos);
 
       if (foundPos == -1) break;
 
       if (normalStr.length >= search.length && normalStr.indexOf(search, pos) + 1) {
 
-        tempItem.weight += (normalStr.length / search.length * 100 > 50) ? 100 + length : normalStr.length / search.length * 100 + length;
+        tempItem.weight += (normalStr.length / search.length * 100 > 50) ? options.weight + length : normalStr.length / search.length * options.weight + length;
         tempItem.in = foundPos;
         tempItem.out = tempItem.in + search.length;
         tempItem.length = search.length;
 
-        if (normalStr.length === search.length) tempItem.weight += 200;
+        if (normalStr.length === search.length) tempItem.weight += options.weight * 2;
 
-        if (normalStr[0] === search[0]) tempItem.weight += 100;
-        
-        
+        if (normalStr[0] === search[0]) tempItem.weight += options.weight;
 
       }
 
@@ -125,6 +132,11 @@ window.substringSearch = function (options) {
 
       pos = foundPos + 1;
 
+    }
+    
+    if (!options.multiply){
+      result = result.sort(compare);
+      result = [result[0]];
     }
 
     return result;
@@ -144,7 +156,7 @@ window.substringSearch = function (options) {
     }
     return comparison;
   }
-  
+
   function copyObject(src) {
     return Object.assign({}, src);
   }
